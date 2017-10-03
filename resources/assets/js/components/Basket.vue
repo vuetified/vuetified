bd054fa761c1b43003844552ea699982a8e2fc96
Uriah Galang <g0ld3lux@gmail.com>
2017-10-03 19:09:01 +0800

    fix but no scroller yet

--- resources/assets/js/components/Basket.vue
+++ resources/assets/js/components/Basket.vue
@@ -1,53 +1,103 @@
 <template>
- <v-data-table
-    :headers="headers"
-    :items="items"
-    v-model="selected"
-    selected-key="id"
-    select-all
-    hide-actions
-    no-data-text="You Have No Orders Yet, Continue Shopping..."
-    class="info--text"
-  >
-    <template slot="headerCell" scope="props">
-      <span v-tooltip:bottom="{ 'html': props.header.text }">
-        {{ props.header.text }}
-      </span>
-    </template>
-    <template slot="items" scope="props">
-      <td>
-        <v-checkbox
-          color="primary"
-          hide-details
-          v-model="props.selected"
-        ></v-checkbox>
-      </td>
-      <td class="title text-xs-left info--text">{{ props.item.id }}</td>
-      <td class="title text-xs-left info--text">{{ props.item.name }}</td>
-      <td class="title text-xs-left info--text">{{ props.item.price | currency('₱') }}</td>
-      <td class="title text-xs-left info--text">{{ props.item.qty }}</td>
-      <td class="title text-xs-left info--text">{{ props.item.subtotal | currency('₱') }}</td>
-    </template>
-    <template slot="footer">
-        <td class="title text-xs-left info--text"></td>
-        <td class="title text-xs-left info--text"></td>
-        <td class="title text-xs-left info--text"></td>
-        <td class="title text-xs-left info--text"></td>
-        <td class="title text-xs-left info--text"></td>
-        <td class="title text-xs-left primary--text">
-            <p style="margin-left:-100px;">Tax :<span style="margin-left:78px;">{{ tax | currency('₱') }}</span></p>
-            <p style="margin-left:-100px;">Sub-Total :<span style="margin-left:24px;">{{ subtotal | currency('₱') }}</span></p>
-            <p style="margin-left:-100px;">Total :<span style="margin-left:67px;">{{ total | currency('₱') }}</span></p>
-        </td>
-    </template>
-  </v-data-table>
+<v-app>
+    <main>
+       <v-container fluid pa-0 ma-0>
+           <v-layout row>
+
+               <v-data-table
+                :headers="headers"
+                :items="items"
+                v-model="selected"
+                selected-key="id"
+                select-all
+                no-data-text="You Have No Orders Yet, Continue Shopping..."
+                class="info--text"
+            >
+                <template slot="headerCell" scope="props">
+                <span v-tooltip:bottom="{ 'html': props.header.text }">
+                    {{ props.header.text }}
+                </span>
+                </template>
+                <template slot="items" scope="props">
+                <td>
+                    <v-checkbox
+                    color="primary"
+                    hide-details
+                    v-model="props.selected"
+                    ></v-checkbox>
+                </td>
+                <td class="title text-xs-left info--text">{{ props.item.id }}</td>
+                <td class="title text-xs-left info--text">{{ props.item.name }}</td>
+                <td class="title text-xs-left info--text">{{ props.item.price | currency(currency) }}</td>
+                <td class="title text-xs-left info--text">{{ props.item.qty }}</td>
+                <td class="title text-xs-left info--text">{{ props.item.subtotal | currency(currency) }}</td>
+                <td class="title text-xs-center info--text">
+                    <v-edit-dialog
+                    @open="tmp = props.item"
+                    @save="updateCartItem(tmp)"
+                    large
+                    lazy
+                    >
+
+                    <v-btn icon>
+                        <v-icon class="teal--text text--lighten-2">fa-edit</v-icon>
+                    </v-btn>
+
+                    <div slot="input" class="mt-3 title primary--text">Update Qty</div>
+
+                    <v-text-field
+                    slot="input"
+                    label="Edit"
+                    v-model="tmp.qty"
+                    single-line
+                    counter
+                    autofocus
+                    :rules="[maxCount]"
+                    >
+                    </v-text-field>
+
+                    </v-edit-dialog>
+                </td>
+                <td class="title text-xs-center">
+                    <v-btn icon @click.native="removeFromCart(props.item.id)">
+                        <v-icon class="red--text text--lighten-2">delete_forever</v-icon>
+                    </v-btn>
+                </td>
+                </template>
+            </v-data-table>
+           </v-layout>
+           <v-flex xs12 class="text-xs-right">
+                 <v-chip label class="red lighten-2 white--text title">
+                    <v-icon left>fa-percent</v-icon> Tax : {{ currency }} {{ tax }}
+                </v-chip>
+            </v-flex>
+
+            <v-flex xs12 class="text-xs-right">
+                <v-chip label class="info white--text title">
+                    <v-icon left>shopping_basket</v-icon> Subtotal : {{ currency }} {{ subtotal }}
+                </v-chip>
+            </v-flex>
+
+            <v-flex xs12 class="text-xs-right">
+                <v-chip label class="primary white--text title">
+                    <v-icon left>fa-money</v-icon> Total : {{ currency }} {{ total }}
+                </v-chip>
+            </v-flex>
+       </v-container>
+     </main>
+
+  </v-app>
 </template>
 
 <script>
+import { createNamespacedHelpers } from 'vuex'
+const { mapActions, mapGetters } = createNamespacedHelpers('cart')
+
 export default {
-    props: ['cart'],
     data () {
         return {
+            currency: '₱',
+            /* Table Specific */
             search: '',
             selected: [],
             headers: [
@@ -55,16 +105,29 @@ export default {
                 { text: 'Product Name', value: 'name', align: 'left' },
                 { text: 'Product Price', value: 'price', align: 'left' },
                 { text: 'Quantity', value: 'qty', align: 'left' },
-                { text: 'Total', value: 'total', align: 'left' }
+                { text: 'Total', value: 'total', align: 'left' },
+                { text: 'Update', align: 'center', sortable: false },
+                { text: 'Delete', align: 'center', sortable: false }
             ],
+            /* Cart Specific */
             items: [],
             tax: 0,
             subtotal: 0,
             total: 0,
-            count: 0
+            count: 0,
+            maxCount: (v) => parseInt(v) <= 999 || 'Max Qty is 999',
+            /* current updated item */
+            tmp: ''
         }
     },
     computed: {
+        ...mapGetters({
+            getItems: 'getItems',
+            getTax: 'getTax',
+            getSubTotal: 'getSubTotal',
+            getTotal: 'getTotal',
+            getCount: 'getCount'
+        }),
         avatarSize () {
             return `${this.size}px`
         },
@@ -74,20 +137,56 @@ export default {
     },
     mounted () {
         let self = this
-        self.items = self.cart.items
-        self.tax = self.cart.tax
-        self.subtotal = self.cart.subtotal
-        self.total = self.cart.total
-        self.count = self.cart.count
+        self.items = Object.values(self.getItems)
+        self.selected = self.items
+        self.tax = self.getTax
+        self.subtotal = self.getSubTotal
+        self.total = self.getTotal
+        self.count = self.getCount
     },
     methods: {
+        ...mapActions({
+            removeItem: 'removeItem', /* params: product.id , request: rowId */
+            destroyCart: 'destroyCart',
+            updateItem: 'updateItem' /* params: product.id and product.qty, request: rowId and qty */
+        }),
+        updateCartItem (tmp) {
+            let payload = {qty: tmp.qty, id: tmp.id}
+            this.updateItem(payload)
+        },
+        emptyCart () {
+            let self = this
+            self.destroyCart()
+        },
+        removeFromCart (id) {
+            let self = this
+            self.removeItem(id)
+        },
         closeCart () {
             Bus.$emit('close-cart')
+        }
+    },
+    watch: {
+        getTax (newValue) {
+            let self = this
+            self.tax = newValue
+        },
+        getCount (newValue) {
+            let self = this
+            self.count = newValue
+        },
+        getItems (newValue) {
+            let self = this
+            self.items = Object.values(newValue)
+        },
+        getSubTotal (newValue) {
+            let self = this
+            self.subtotal = newValue
+        },
+        getTotal (newValue) {
+            let self = this
+            self.total = newValue
         }
     }
 }
 </script>
-
-<style>
-
-</style>
--- resources/assets/js/components/Cart.vue
+++ resources/assets/js/components/Cart.vue
@@ -10,11 +10,11 @@
           <v-spacer></v-spacer>
           <v-toolbar-items>
               <!-- If There is no User Account Login Yet Redirect to Authentication Page -->
-            <v-btn class="success--text" flat @click.native="checkout()" v-if="cart.items.length > 0">Checkout<v-icon right dark>payment</v-icon></v-btn>
+            <v-btn class="success--text" flat @click.native="checkout()" v-if="count > 0">Checkout<v-icon right dark>payment</v-icon></v-btn>
             <v-btn class="warning--text" flat @click.native="redirectBack()" v-else>Close</v-btn>
           </v-toolbar-items>
         </v-toolbar>
-        <basket :cart="cart"></basket>
+        <basket></basket>
       </v-card>
     </modal>
 </template>
@@ -22,24 +22,14 @@
 <script>
 import Basket from './Basket.vue'
 import { createNamespacedHelpers } from 'vuex'
-const { mapActions, mapGetters, mapMutations } = createNamespacedHelpers('cart')
+const { mapGetters } = createNamespacedHelpers('cart')
 
 export default {
     data: () => ({
-        cart: {
-            items: [],
-            tax: 0,
-            subtotal: 0,
-            total: 0,
-            count: 0
-        }
+        count: 0
     }),
     computed: {
         ...mapGetters({
-            getItems: 'getItems',
-            getTax: 'getTax',
-            getSubTotal: 'getSubTotal',
-            getTotal: 'getTotal',
             getCount: 'getCount'
         }),
         isDark () {
@@ -51,35 +41,13 @@ export default {
     },
     mounted () {
         let self = this
-        self.cart = {
-            items: Object.values(self.getItems),
-            tax: self.getTax,
-            subtotal: self.getSubTotal,
-            total: self.getTotal,
-            count: self.getCount
-        }
+        self.count = self.getCount
         self.$modal.show('cart-modal')
         Bus.$on('close-cart', () => {
             self.redirectBack()
         })
     },
     methods: {
-        ...mapActions({
-            addItem: 'addItem',
-            removeItem: 'removeItem',
-            destroyCart: 'destroyCart',
-            updateItem: 'updateItem'
-        }),
-        ...mapMutations({
-            setItems: 'setItems',
-            setTax: 'setTax',
-            setSubTotal: 'setSubTotal',
-            setTotal: 'setTotal',
-            newCartForm: 'newForm'
-        }),
-        exceedOrderLimit () {
-            return this.cart.items.length > 999
-        },
         redirectBack () {
             let self = this
             self.$router.push({path: self.$store.state.route.from.fullPath})
@@ -87,9 +55,12 @@ export default {
         checkout () {
             let self = this
             return self.$nextTick(() => self.$router.push({ name: 'checkout' }))
-            // show shipping details page , for non auth users.
-            // verify in the server for express checkout the email.
-            // if registered email then 
+        }
+    },
+    watch: {
+        getCount (newValue) {
+            let self = this
+            self.count = newValue
         }
     }
 }
