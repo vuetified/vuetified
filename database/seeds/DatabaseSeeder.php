b6cf638013cd9570c4404d16ba1f3ae39013ef60
Uriah Galang <vuetified@gmail.com>
2017-10-20 17:28:56 +0800

    added gateway and courier seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(GatewaySeeder::class);
+        $this->call(CourierSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(RolesAndPermissionsSeeder::class);
