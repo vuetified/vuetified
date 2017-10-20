bdb5dbdf9466f89c848404c9ed4ec5f94244dcd9
Uriah Galang <vuetified@gmail.com>
2017-10-20 17:43:23 +0800

    added gateways and couriers route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 /* Cart Web Api */
+Route::get('/gateways', function(){
+   return  \App\Gateway::all();
+})->name('api.gateway.index');
+Route::get('/couriers', function(){
+    return  \App\Courier::all();
+ })->name('api.courier.index');
 Route::post('/cart/add', 'Api\Cart\CartController@add')->name('api.cart.add');
 Route::post('/cart/update', 'Api\Cart\CartController@update')->name('api.cart.update');
 Route::post('/cart/delete', 'Api\Cart\CartController@delete')->name('api.cart.delete');
