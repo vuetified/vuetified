16f19b629b76565c554ba01577cfac51d13f2cfe
Uriah Galang <vuetified@gmail.com>
2017-10-23 16:07:36 +0800

    remove class that is not needed

--- app/Http/Controllers/Api/Order/OrderController.php
+++ app/Http/Controllers/Api/Order/OrderController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Api\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Order;
-use App\Gateway;
-use App\Courier;
 use Cart;
 
 class OrderController extends Controller
@@ -17,30 +15,32 @@ class OrderController extends Controller
     }
     public function create(Request $request)
     {
-        
+        /* create new Order */
         $order = new Order();
         $order->user_id = optional($request->user())->id;
         $order->cart = json_encode($request->cart);
         $order->customer_details = json_encode($request->customer_details);
-        $order->shipping_details =json_encode($request->shipping_details);
+        if($request->has('shipping_details')){
+            $order->shipping_details =json_encode($request->shipping_details);
+        }
+        /* create new Payment */
         $mop = new $request->mop['model'];
         $mop->gateway_id = $request->mop['id'];
         $mop->amount = $request->cart['total'];
         $mop->save();
         $mop->payments()->save($order);
+        /* create new Courier */
         $courier = new $request->courier['model'];
         $courier->courier_id = $request->courier['id'];
         if($request->has('shipping_fee')){
-        $courier->shipping_fee = $request->shipping_fee;
+            $courier->shipping_fee = $request->shipping_fee;
         }
         $courier->save();
         $courier->shipments()->save($order);
-
+        /* Destroy Cart */
         Cart::destroy();
 
         return response()->json([
-            // 'order' => $order,
-            // 'mop' => $mop,
             'message' => 'Order Placed!'
         ],200);
     }
