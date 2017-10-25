774ebd8e8dce3d6db71b167b9bc9d34ceb1def0a
Uriah Galang <vuetified@gmail.com>
2017-10-25 17:40:32 +0800

    added Dashboard panel

--- resources/assets/js/pages/Dashboard.vue
+++ resources/assets/js/pages/Dashboard.vue
@@ -1,6 +1,30 @@
 <template>
-  <main-layout :class="[contentClass]">
-    <p>Dashboard page</p>
+  <main-layout :class="[contentClass]" :style="{ paddingTop: `100px` }">
+    <v-container  fluid>
+      <v-layout row wrap>
+        <!-- make a card to display this -->
+        <v-flex xs12 md4 text-xs-center>
+            <h3 class="primary--text">Total: {{ total }}</h3>
+        </v-flex>
+        <v-flex xs12 md4 text-xs-center>
+            <h3 class="primary--text">Unpaid: {{ unpaid }} </h3>
+        </v-flex>
+        <v-flex xs12 md4 text-xs-center>
+            <h3 class="primary--text">Confirmed Paid: {{ paid }}</h3>
+        </v-flex>
+        <v-flex xs12 md4 text-xs-center>
+            <h3 class="primary--text">Delivered: {{ sent }} </h3>
+        </v-flex>
+        <v-flex xs12 md4 text-xs-center>
+            <h3 class="primary--text">Confirmed Received: {{ received }}</h3>
+        </v-flex>
+        <v-flex xs12 md4 text-xs-center>
+            <h3 class="primary--text">Done: {{ done }} </h3>
+        </v-flex>
+      </v-layout>
+      <!-- Table to View All Orders -->
+      <!-- Modal To Load Each Order Details -->
+    </v-container>
   </main-layout>
 </template>
 
@@ -11,7 +35,13 @@ import Theme from '../mixins/theme'
 export default {
     mixins: [Theme],
     data: () => ({
-        contentClass: { 'grey': true, 'lighten-4': true, 'accent--text': true }
+        contentClass: { 'grey': true, 'lighten-4': true, 'accent--text': true },
+        total: 10,
+        unpaid: 2,
+        paid: 5,
+        sent: 4,
+        received: 3,
+        done: 3
     }),
     components: {
         MainLayout
