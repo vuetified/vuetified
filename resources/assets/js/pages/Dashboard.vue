9d00bcd84f394f58fb2455d6a3826d982dce765e
Uriah Galang <vuetified@gmail.com>
2017-10-31 06:05:56 +0800

    added todo and fix typo in dashboard

--- resources/assets/js/pages/Dashboard.vue
+++ resources/assets/js/pages/Dashboard.vue
@@ -184,7 +184,7 @@ export default {
             strg = strg.replace(/[^0-9$.,]/g, '')
             if (strg.indexOf(',') > strg.indexOf('.')) decimal = ','
             if ((strg.match(new RegExp('\\' + decimal, 'g')) || []).length > 1) decimal = ''
-            if (decimal != '' && (strg.length - strg.indexOf(decimal) - 1 == 3) && strg.indexOf('0' + decimal) !== 0) decimal = ''
+            if (decimal !== '' && (strg.length - strg.indexOf(decimal) - 1 == 3) && strg.indexOf('0' + decimal) !== 0) decimal = ''
             strg = strg.replace(new RegExp('[^0-9$' + decimal + ']', 'g'), '')
             strg = strg.replace(',', '.')
             return parseFloat(strg)
