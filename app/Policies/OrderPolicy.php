4538597f073aa17b5428af3d6cad78f6d21da2d3
Uriah Galang <vuetified@gmail.com>
2017-10-31 17:39:54 +0800

    added OrderPolicy

---
+++ app/Policies/OrderPolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Order;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class OrderPolicy
+{
+    use HandlesAuthorization;
+
+    public function before($user, $ability)
+    {
+        if ($user->isSuperAdmin()) {
+            return true;
+        }
+    }
+    /**
+     * Determine whether the user can view the order.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function view(User $user, Order $order)
+    {
+        return $user->id === $order->user_id;
+    }
+
+    /**
+     * Determine whether the user can create orders.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the order.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function update(User $user, Order $order)
+    {
+        return $user->id === $order->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the order.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Order  $order
+     * @return mixed
+     */
+    public function delete(User $user, Order $order)
+    {
+        return $user->id === $order->user_id;
+    }
+}
