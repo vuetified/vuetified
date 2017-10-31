bebca653710d6276561b29108e9730a11a40d109
Uriah Galang <vuetified@gmail.com>
2017-10-31 17:36:10 +0800

    change message

--- app/Exceptions/OrderDone.php
+++ app/Exceptions/OrderDone.php
@@ -7,6 +7,6 @@ class OrderDone extends \Exception
 {
     public function render($request)
     {
-        return response()->json(['message' => 'Failed To Update Order#'.$request->order],404);
+        return response()->json(['message' => 'Undone Order#'.$request->order.' To Update'],404);
     }
 }
