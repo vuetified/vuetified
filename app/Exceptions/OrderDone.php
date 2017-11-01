f6851da03299d7404a926abc79a827b2809573e2
Uriah Galang <vuetified@gmail.com>
2017-11-01 10:01:28 +0800

    use route model bind order var

--- app/Exceptions/OrderDone.php
+++ app/Exceptions/OrderDone.php
@@ -7,6 +7,6 @@ class OrderDone extends \Exception
 {
     public function render($request)
     {
-        return response()->json(['message' => 'Undone Order#'.$request->order.' To Update'],404);
+        return response()->json(['message' => 'Undone Order#'.$request->order->id.' To Update'],404);
     }
 }
