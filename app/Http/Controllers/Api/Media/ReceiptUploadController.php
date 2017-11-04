8bf4b54e9f8c51db45aae7e9af96ef4eaa373da1
Uriah Galang <vuetified@gmail.com>
2017-11-04 14:08:55 +0800

    added isMarkDone check and can update order

--- app/Http/Controllers/Api/Media/ReceiptUploadController.php
+++ app/Http/Controllers/Api/Media/ReceiptUploadController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers\Api\Media;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Order;
+use App\Exceptions\OrderArchive;
 
 class ReceiptUploadController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:api']);
+        $this->middleware(['auth:api','can:update,order']);
     }
 
     public function upload(Order $order, Request $request)
     {
+        $this->isMarkDone($order);
         $file = $order->getMedia('receipts')->first();
         if(!$file){
             /* add new receipt */
@@ -36,5 +38,12 @@ class ReceiptUploadController extends Controller
             'order' => $order,
             'message' => 'Receipt Uploaded'
         ],200);
+    }
+
+    private function isMarkDone($order)
+    {
+        if($order && $order->done){
+            throw new OrderArchive;
+        }
     }
 }
