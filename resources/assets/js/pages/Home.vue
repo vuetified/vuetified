cb485109eefd5f97a6178724c439267501d763f6
Uriah Galang <vuetified@gmail.com>
2017-11-05 17:57:46 +0800

    added Video Galler

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -1,8 +1,27 @@
 <template>
   <main-layout :class="[contentClass]">
     <carousel></carousel>
-    <!-- <video-gallery></video-gallery> -->
     <show-case></show-case>
+    <v-layout row wrap>
+        <v-flex xs12 text-xs-center>
+            <youtube :video-id="youtube_id" :player-width="youtubeWidth" :player-height="youtubeHeight"></youtube>
+        </v-flex>
+    </v-layout>
+    <div class="pa-3">
+
+    </div>
+    <v-layout row justify-center>
+        <v-flex xs4 v-for="(video,key) in videos" :key="key" text-xs-center>
+            <v-card>
+                <v-card-media :src="video.poster"
+                :style="{ backgroundImage: 'url(' + video.poster + ')', height: imageHeight }"
+                style="cursor:pointer;"
+                @click="changeVideo(video)"
+                >
+                </v-card-media>
+            </v-card>
+        </v-flex>
+    </v-layout>
     <logo-slider></logo-slider>
     <footer-note></footer-note>
   </main-layout>
@@ -19,12 +38,72 @@ import Theme from '../mixins/theme'
 
 export default {
     mixins: [Theme],
+    computed: {
+        imageHeight () {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return '100px'
+            case 'sm': return '100px'
+            case 'md': return '150px'
+            case 'lg': return '250px'
+            case 'xl': return '250px'
+            }
+        },
+        youtubeHeight () {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return '315px'
+            case 'sm': return '315px'
+            case 'md': return '450px'
+            case 'lg': return '750px'
+            case 'xl': return '864px'
+            }
+        },
+        youtubeWidth () {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return `${window.innerWidth}px`
+            case 'sm': return `${window.innerWidth}px`
+            case 'md': return `${window.innerWidth}px`
+            case 'lg': return `${window.innerWidth}px`
+            case 'xl': return `${window.innerWidth}px`
+            }
+        }
+    },
     data: () => ({
         contentClass: { 'white': true, 'accent--text': true },
-        backgroundcolor: 'white'
+        backgroundcolor: 'white',
+        videos: [
+            {
+                title: 'Testimonials',
+                href: 'https://www.youtube.com/watch?v=l-nKCcfSMHc',
+                type: 'text/html',
+                youtube: 'l-nKCcfSMHc',
+                poster: 'https://img.youtube.com/vi/l-nKCcfSMHc/maxresdefault.jpg'
+            },
+            {
+                title: 'Opportunity',
+                href: 'https://www.youtube.com/watch?v=Y65XWelZiB8',
+                type: 'text/html',
+                youtube: 'Y65XWelZiB8',
+                poster: 'https://img.youtube.com/vi/Y65XWelZiB8/maxresdefault.jpg'
+            },
+            {
+                title: 'Product Presentation',
+                href: 'https://www.youtube.com/watch?v=DjrAVpRNKTo',
+                type: 'text/html',
+                youtube: 'DjrAVpRNKTo',
+                poster: 'https://img.youtube.com/vi/DjrAVpRNKTo/maxresdefault.jpg'
+            }
+        ],
+        youtube_id: 'l-nKCcfSMHc',
+        height: null
     }),
     mounted () {
         Bus.$emit('footer-content-visible', true)
+    },
+    methods: {
+        changeVideo (video) {
+            this.youtube_id = video.youtube
+        }
+
     },
     components: {
         MainLayout,
@@ -36,3 +115,12 @@ export default {
     }
 }
 </script>
+<style scoped>
+.image {
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    border: 2px solid #BA9A5A;
+    margin: 15px;
+  }
+</style>
