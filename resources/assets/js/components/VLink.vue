00863c093b91e08a3cefee69a31166e43ec19abe
Uriah Galang <vuetified@gmail.com>
2017-11-08 20:42:19 +0800

    added a check for link , then by default uses vrouter path

--- resources/assets/js/components/VLink.vue
+++ resources/assets/js/components/VLink.vue
@@ -76,7 +76,18 @@ export default {
     },
     methods: {
         navigate (href) {
-            this.$router.push({ path: `${href}` })
+            let self = this
+            /* if valid url */
+            if (self.isURL(href)) {
+                window.open(href)
+            } else { /* when using vue router path */
+                this.$router.push({ path: `${href}` })
+            }
+        },
+        isURL (str) {
+            var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$'
+            var url = new RegExp(urlRegex, 'i')
+            return str.length < 2083 && url.test(str)
         }
 
     }
