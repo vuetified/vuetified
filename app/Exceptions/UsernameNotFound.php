a0a6f931c26faf98fb7bd4c8e4e54e1c1cae35f3
Uriah Galang <vuetified@gmail.com>
2017-11-11 07:36:27 +0800

    added a redirect if request is not json

--- app/Exceptions/UsernameNotFound.php
+++ app/Exceptions/UsernameNotFound.php
@@ -7,6 +7,9 @@ class UsernameNotFound extends \Exception
 {
     public function render($request)
     {
+        if($request->wantsJson()){
         return response()->json(['message' => 'Username Not Found Exception: '.$request->username.' not found.'],404);
+        }
+        return redirect(\Config::get('app.url'));
     }
 }
