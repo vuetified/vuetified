3381a2d03d1808abb8c40726353e05af64cd7b05
Uriah Galang <vuetified@gmail.com>
2017-11-11 08:46:09 +0800

    remove cookie

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -8,18 +8,11 @@ class DomainController extends Controller
 {
     public function sponsor($username)
     {
-        if(!request()->cookie('sponsor')){
-            \Cookie::queue(\Cookie::make('sponsor', $username->id));
-         }
-        return view('app')->withCookie(cookie('sponsor', $username));
+        return view('app');
     }
 
     public function app()
     {
-        // Default Sponsor
-        if(!request()->cookie('sponsor')){
-        \Cookie::queue(\Cookie::make('sponsor', \Vuetified::user()->first()->id));
-        }
         return view('app');
     }
 }
