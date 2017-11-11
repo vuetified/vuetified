b83a4ea5b424f4815a967703d209fa3f44856420
Uriah Galang <vuetified@gmail.com>
2017-11-11 14:26:00 +0800

    added setttings and users"

--- resources/assets/js/routes/profileRoutes.js
+++ resources/assets/js/routes/profileRoutes.js
@@ -1,5 +1,7 @@
 const Checkout = () => import('../pages/Checkout.vue')
 const Dashboard = () => import('../pages/Dashboard.vue')
+const Settings = () => import('../pages/Settings.vue')
+const Users = () => import('../pages/Users.vue')
 
 export default [
     /* Start Authenticated Routes */
@@ -14,9 +16,29 @@ export default [
         }
     },
     {
+        path: '/settings',
+        component: Settings,
+        name: 'settings',
+        meta: {
+            requiresAuth: true,
+            permission: 'guest',
+            fail: '/error'
+        }
+    },
+    {
         path: '/checkout',
         component: Checkout,
         name: 'checkout',
+        meta: {
+            requiresAuth: true,
+            permission: 'guest',
+            fail: '/error'
+        }
+    },
+    {
+        path: '/users',
+        component: Users,
+        name: 'users',
         meta: {
             requiresAuth: true,
             permission: 'guest',
