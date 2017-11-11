e32af03c27794cb52feed1303278230637799405
Uriah Galang <vuetified@gmail.com>
2017-11-11 14:25:19 +0800

    added company profile added, user crud, added settings

--- resources/assets/js/partials/LeftSideBar.vue
+++ resources/assets/js/partials/LeftSideBar.vue
@@ -10,14 +10,19 @@
         <!-- V-For Links From Menu -->
         <v-link :dark="darkClass" v-for="link in links" :key="link.id" :title="link.title" :href="link.href" :icon="link.action"></v-link>
         <!-- Individual Link (Custom Additional) -->
+        <v-link :dark="darkClass"  title="Company Profile" :href="'/about'"   icon="fa-building"></v-link>
         <v-link :dark="darkClass"  title="Products" :href="'/products'"   icon="fa-shopping-basket"></v-link>
         <v-link :dark="darkClass"  title="Categories" :href="'/categories'"   icon="fa-tag"></v-link>
         <!-- Expandable Group Links from Group Link -->
         <category-link :dark="darkClass" :items="grouplinks"></category-link>
         <v-subheader :class="{'blue-grey--text': !isDark, 'text--lighten-1': !isDark, 'white--text': isDark}">Members Area</v-subheader>
+        <!-- Admin Only Accessible -->
+        <v-link v-if="getMe.isAdmin" :dark="darkClass"  title="User Management"  :href="'/users'" icon="fa-users"></v-link>
+        <!-- Normal User Links -->
         <v-link v-if="getAuth" :dark="darkClass"  title="Dashboard"  :href="'/dashboard'" icon="dashboard"></v-link>
+        <v-link v-if="getAuth" :dark="darkClass"  title="Settings"  :href="'/settings'" icon="fa-cogs"></v-link>
         <v-link v-if="getAuth" :dark="darkClass"  title="Logout"  :href="'/logout'" icon="power_settings_new"></v-link>
-
+        <!-- Guest Links -->
         <v-link v-if="!getAuth" :dark="darkClass"  title="Login"  :href="'/login'" icon="fa-key"></v-link>
         <v-link v-if="!getAuth" :dark="darkClass"  title="Register"  :href="'/register'" icon="fa-user-plus"></v-link>
       </v-list>
@@ -41,7 +46,8 @@ export default {
     }),
     computed: {
         ...mapGetters({
-            getAuth: 'getAuth'
+            getAuth: 'getAuth',
+            getMe: 'getMe'
         })
     },
     components: {
