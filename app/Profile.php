65a681eaf4fb2cdde7594c337463fac7c1d0ac14
Uriah Galang <vuetified@gmail.com>
2017-11-12 13:37:49 +0800

    added fillable filleds

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+
+    protected $fillable = [
+        'first_name', 'last_name', 'contact_no', 'address_1', 'address_2', 'city', 'country', 'zip_code', 'state_province'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
