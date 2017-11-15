bfd7c8bb22f8f3545b4ee05e11bddbac88f0d0af
Uriah Galang <vuetified@gmail.com>
2017-11-16 06:37:51 +0800

    added check if a model is present if none show an error that no model associated

--- app/Http/Controllers/Api/Order/OrderDetailsConstroller.php
+++ app/Http/Controllers/Api/Order/OrderDetailsConstroller.php
@@ -44,31 +44,45 @@ class OrderDetailsConstroller extends Controller
     {
         $this->isMarkDone($order);
         $gateway = $order->payment;
-        $gateway->transaction_no = $request->transaction_no;
-        $gateway->account_name = $request->account_name;
-        $gateway->account_no = $request->account_no;
-        $gateway->amount = $request->amount;
-        $gateway->currency = $request->currency;
-        $gateway->date_paid = $request->date_paid;
-        $gateway->save();
-        return response()->json([
-            'message' => 'Order #'.$order->id.' Updated: Payment Details'
-            ],200);
+        if($gateway){
+            $gateway->transaction_no = $request->transaction_no;
+            $gateway->account_name = $request->account_name;
+            $gateway->account_no = $request->account_no;
+            $gateway->amount = $request->amount;
+            $gateway->currency = $request->currency;
+            $gateway->date_paid = $request->date_paid;
+            $gateway->save();
+            return response()->json([
+                'message' => 'Order #'.$order->id.' Updated: Payment Details'
+                ],200);
+        }else {
+            return response()->json([
+                'message' => 'Order #'.$order->id.' Failed To Update: No Payment Model Associated!'
+                ],200);
+        }
+        
     }
 
     public function updateShipmentDetails(Order $order,Request $request)
     {
         $this->isMarkDone($order);
         $shipment = $order->shipment;
-        $shipment->tracking_no = $request->tracking_no;
-        $shipment->sent = $request->sent;
-        $shipment->date_sent = $request->date_sent;
-        $shipment->received = $request->received;
-        $shipment->date_received = $request->date_received;
-        $shipment->save();
-        return response()->json([
-            'message' => 'Order #'.$order->id.' Updated: Shipment Details'
-            ],200);
+        if($shipment){
+            $shipment->tracking_no = $request->tracking_no;
+            $shipment->sent = $request->sent;
+            $shipment->date_sent = $request->date_sent;
+            $shipment->received = $request->received;
+            $shipment->date_received = $request->date_received;
+            $shipment->save();
+            return response()->json([
+                'message' => 'Order #'.$order->id.' Updated: Shipment Details'
+                ],200);
+        }else{
+            return response()->json([
+                'message' => 'Order #'.$order->id.' Failed To Update: No Shipment Model Associated!'
+                ],400);
+        }
+        
     }
 
     private function isMarkDone($order)
