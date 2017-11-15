2b4ecaf29e60697e9582c01f855eac8b72db045d
Uriah Galang <vuetified@gmail.com>
2017-11-16 06:44:56 +0800

    fix userfactory password double encryption and added username

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,12 +4,11 @@ use Faker\Generator as Faker;
 use App\User;
 
 $factory->define(User::class, function (Faker $faker) {
-    static $password;
-
     return [
         'name' => $faker->name,
+        'username' => $faker->unique()->userName,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => 'secret',
         'remember_token' => str_random(10),
     ];
 });
