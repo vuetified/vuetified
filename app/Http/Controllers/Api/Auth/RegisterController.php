899819797a8f3149162a9d4b3b4afe7189abf33c
Uriah Galang <vuetified@gmail.com>
2017-11-17 08:12:59 +0800

    add logic in creating a new user , adding new profile, role and new link

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Client;
 use App\Traits\IssueTokenTrait;
+use App\Profile;
+use App\Link;
+use App\Rules\CheckSponsor;
 
 class RegisterController extends Controller
 {
@@ -21,19 +24,47 @@ class RegisterController extends Controller
 
     public function register(Request $request)
 	{
+		/* validate request input */
 		request()->validate([
 			'name' => 'required',
     		'username' => 'required|email',
     		'password' => 'required|min:6|confirmed',
-			'password_confirmation' => 'required'
+			'password_confirmation' => 'required',
+			'role' => [
+				'sometimes',
+				'required',
+				'exists:roles,name'
+			],
+			'sponsor' => [
+				'sometimes',
+				'required',
+				new CheckSponsor
+			]
 		]);
-
-    	User::create([
+	    /* create user account */
+    	$user = User::create([
 			'name' => request('name'),
     		'email' => request('username'),
     		'password' => request('password')
-    	]);
+		]);
+		
+		/* create an empty profile */
+		$profile = new Profile();
+		$user->profile()->save($profile);
 
+		/* attach specific role */
+		if ($request->has('role')) {
+			$user->assignRole(request('role'));
+		}
+		/* attach default role */
+		else {
+			$user->assignRole('customer');
+		}
+		/* create a new link for this account */
+		$link = new Link();
+		$link->link = $user->username;
+		$user->referralLink()->save($link);
+		/* issue new access_token */
     	return $this->issueToken($request, 'password');
     }
 }
