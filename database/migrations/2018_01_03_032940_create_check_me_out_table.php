bc53df101e5e0701c96db00d3545e069f5ca207d
Uriah Galang <vuetified@gmail.com>
2018-01-03 14:14:22 +0800

    added checkmeout migrations

---
+++ database/migrations/2018_01_03_032940_create_check_me_out_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCheckMeOutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('check_me_out', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('check_me_out');
+    }
+}
