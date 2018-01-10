a45d5e30e95aeb993f44b3965ecdb8f029a5752d
Uriah Galang <vuetified@gmail.com>
2018-01-10 16:42:06 +0800

    Added actions and state

--- resources/assets/js/store/modules/checkmeout.js
+++ resources/assets/js/store/modules/checkmeout.js
@@ -1,45 +1,104 @@
 const state = {
     /* Check Me Out Authentication */
-    access_token: null,
-    api_key: '',
-    secret_key: ''
+    settings: {
+        id: null,
+        user_id: null,
+        access_token: null,
+        api_key: null,
+        secret_key: null
+    },
+    receptacles:[]
 }
 
 const getters = {
-    getToken: state => state.access_token
+    getToken: state => state.settings.access_token,
+    getID: state => state.settings.id,
+    getUserID: state => state.settings.user_id,
+    getApiKey: state => state.settings.api_key,
+    getSecretKey: state => state.settings.secret_key,
+    getReceptacles: state => state.receptacles
 }
 
 const actions = {
-    // async checkmeout({ commit }, form){
-    //     form.busy = true
-    //     try {
-    //         const payload = (await App.post('https://api.checkmeout.ph/v1/auth/login', form))
-    //         form.busy = false
-    //         commit('setToken', payload.token)
-    //         vm.$popup({ message: 'Check Me Out Integrated', backgroundColor: '#4db6ac', delay: 5, color: '#fffffa' })
-    //         vm.$router.push({ name: 'settings' })
-    //     } catch ({errors, message}) {
-    //         form.errors.set(errors)
-    //         form.busy = false
-    //         vm.$popup({ message: message, backgroundColor: '#e57373', delay: 5, color: '#fffffa' })
-    //     }
-    // }
-    async checkmeout({ commit }, form){
 
-        let payload= (await App.post(route('api.cmo.login'),form))
-        console.log(payload)
-        commit('setToken', payload.checkmeout.token)
-        // payload.checkmeout.api_key
-        // payload.checkmeout.secret_key
-        // payload.checkmeout.user_id
-       
-        
+    async login({ commit }, form){
+        try {
+            form.busy = true
+            let payload= (await App.post(route('api.cmo.login'),form))
+            console.log(payload)
+            commit('setToken', payload.checkmeout.token)
+            commit('setID', payload.checkmeout.id)
+            commit('setUserID', payload.checkmeout.user_id)
+            
+            form.busy = false
+            vm.$popup({ message: payload.message, backgroundColor: '#4db6ac', delay: 5, color: '#fffffa' })
+            vm.$router.push({ name: 'settings' })
+        }catch ({errors, message}) {
+            form.errors.set(errors)
+            form.busy = false
+            vm.$popup({ message: message, backgroundColor: '#e57373', delay: 5, color: '#fffffa' })
+        }
+    },
+    async fetchKeys({ commit }){
+        try{
+            let payload = (await axios.post(route('api.cmo.fetchkeys')))
+            console.log(payload.data)
+            commit('setToken', payload.data.token)
+            commit('setID', payload.data.id)
+            commit('setUserID', payload.data.user_id)
+            commit('setApiKey', payload.data.api_key)
+            commit('setSecretKey', payload.data.secret_key)
+        }catch ({errors, message}) {
+            if(errors) console.log(errors)
+            if(message) console.log(message)
+        }
+    },
+    async addKeys({commit}, form){
+        try {
+            form.busy = true
+            let payload= (await App.post(route('api.cmo.addKeys'),form))
+            console.log(payload)
+            commit('setApiKey', payload.api_key)
+            commit('setSecretKey', payload.secret_key)
+            form.busy = false
+            vm.$popup({ message: payload.message, backgroundColor: '#4db6ac', delay: 5, color: '#fffffa' })
+        }catch ({errors, message}) {
+            if(errors) form.errors.set(errors)
+            if(message) vm.$popup({ message: message, backgroundColor: '#e57373', delay: 5, color: '#fffffa' })
+            form.busy = false
+        }
+    },
+    async fetchReceptacles({commit}){
+        try{
+            let payload = (await axios.get(route('api.cmo.getReceptacles')))
+            console.log(payload.data)
+            commit('setReceptacles', payload.data)
+        }catch ({errors, message}) {
+            if(errors) console.log(errors)
+            if(message) console.log(message)
+        }
     }
 }
 
 const mutations = {
     setToken: (state, payload) => {
-        state.access_token = payload
+        state.settings.access_token = payload
+    },
+    setID: (state, payload) => {
+        state.settings.id = payload
+    },
+    setUserID: (state, payload) => {
+        state.settings.user_id = payload
+    },
+    setApiKey: (state, payload) => {
+        state.settings.api_key = payload
+    },
+    setSecretKey: (state, payload) => {
+        state.settings.secret_key = payload
+    },
+    setReceptacles: (state, payload) => {
+        state.receptacles = []
+        state.receptacles = payload
     }
 }
 
