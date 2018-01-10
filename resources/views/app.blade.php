ef70dc3e51863db496d9bf386e1423d85dc1340b
Uriah Galang <vuetified@gmail.com>
2018-01-10 16:37:15 +0800

    added meta tag for description

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta id="desc" name="description" content="{{ config('app.site.description') }}">
+<link rel="canonical" id="canonical" href="/">
 <!-- Fix For Error: Loading chunk [chunk-no] failed at HTMLScriptElement.onScriptComplete -->
 <base href="/" />
 @endpush
