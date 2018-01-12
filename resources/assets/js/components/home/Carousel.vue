78b4d65b5596fd708d18cebd4a9bc77ee04fb44f
Uriah Galang <vuetified@gmail.com>
2018-01-12 21:07:34 +0800

    added checkout process image

--- resources/assets/js/components/home/Carousel.vue
+++ resources/assets/js/components/home/Carousel.vue
@@ -9,14 +9,25 @@
       :key="i"
     >
       <div class="headline text-xs-center">
-        <h3 class="white--text"> <span >{{ item.headline }}</span></h3>
-        <p class="white--text title" ><span >{{ item.subheader }}</span></p>
+        <h3 
+          class="white--text" 
+          v-if="item.headline"
+        > 
+        <span >{{ item.headline }}</span></h3>
+        <p 
+          class="white--text title"
+          v-if="item.subheader" 
+        ><span >{{ item.subheader }}</span></p>
         <v-btn 
           class="primary white--text" 
           @click.native.prevent="goToLink(item.buttonlink)"
+          v-if="item.buttontext"
         >
           {{ item.buttontext }} 
-          <v-icon right dark>{{ item.icon }}</v-icon>
+          <v-icon 
+            right 
+            dark
+          >{{ item.icon }}</v-icon>
         </v-btn>
       </div>
     </v-carousel-item>
@@ -32,6 +43,7 @@ export default {
             {src: '/img/parallax1.jpg', headline: 'Amazing Organic Health Products', 'subheader': 'Choose From A Wide Variety of Health And Organic Products', 'buttontext': 'See All Products', 'buttonlink': '/products', 'icon': 'shopping_basket'},
             {src: '/img/parallax2.jpg', headline: 'Competetive Low Cost Foodcart Franchise', 'subheader': 'Start For As Low As P73,600  ONLY!', 'buttontext': 'Be A Franchisee Now!', 'buttonlink': '/categories/food-cart', 'icon': 'store_mall_directory'},
             {src: '/img/parallax3.jpg', headline: 'Want To Resell Our Product?', 'subheader': 'Why Not Get Paid For Referring New Consumer?', 'buttontext': 'Be A Reseller', 'buttonlink': '/register', 'icon': 'person_pin'},
+            {src: '/img/checkoutprocess.jpg', headline: 'Easy Shopping Experience', subheader: 'So Many Ways To Pay, Credit Card, Online Payment, Bank Deposit and COD', 'buttontext': 'Shop Now', 'buttonlink': '/products', 'icon': 'fa-shopping-bag '},
             {src: '/img/parallax4.jpg', headline: 'Interested But Still Undecided?', 'subheader': 'Our Customer Service Is Open For Your Questions', 'buttontext': 'Contact Us', 'buttonlink': '/support', 'icon': 'textsms'}
         ]
     }),
