c5678c03dd59e9bb4575b74f17c54d7f0c636bc3
Uriah Galang <vuetified@gmail.com>
2018-01-30 06:56:18 +0800

    add chunks hash on dynamic import route

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,11 @@ let mix = require('laravel-mix')
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    output: {
+        chunkFilename: mix.inProduction() ? '[name].[chunkhash].js' : '[name].js'
+    }
+})
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .extract([
